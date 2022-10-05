6b3fce345d539e00f160e02bc85e936080d3166b
Jalen Arrington <jmarrington178@yahoo.com>
2022-10-05 17:24:04 -0400

    Added a searchbar into the overhead navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,8 @@
-import "./Navbar.css"
+import App from "../../App";
+import SearchBar from "../SearchBar/SearchBar";
+import '../SearchBar/SearchBar.css';
+import "./Navbar.css";
+import getSearchedVideoGame from "../../App";
 
 const Navbar = () => {
     return ( 
@@ -7,6 +11,7 @@ const Navbar = () => {
                 <li className="brand">
                     <div style={{ textDecoration: "none", color: "white" }}>
                         <b>Game Data</b>
+                        <SearchBar submittedSearchTerm={getSearchedVideoGame} />
                     </div>
                 </li>
             </ul>
