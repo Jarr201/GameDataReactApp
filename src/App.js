f5e04cde8452227ac2cb5f0d479ecea76eee7e6a
irishandyesiam <nathan.irish@gmail.com>
2022-10-05 14:07:26 -0500

    async function getAllVideoGames created. Instanciated DisplayAll_VG. Will need to change names per user story goals.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+import DisplayAll_VG from './Components/DisplayAll_VG/DisplayAll_VG';
 import './App.css';
 
 function App() {
+
+  const [videogames, setVideogames] = useState([]);
+
+  useEffect(() => {
+    getAllVideoGames();
+  }, [])
+
+  async function getAllVideoGames(){
+    let response = await axios.get('http://localhost:8080/all');
+    setVideogames(response.data);
+    console.log(response.data);
+  }
+
+  
+
   return (
     <div>
       <h3>Hello World</h3>
+      <DisplayAll_VG videogames={videogames}/>
     </div>
   );
 }
---
+++ src/Components/DisplayAll_VG/DisplayAll_VG.jsx
@@ -0,0 +1,11 @@
+
+import { Chart } from 'react-google-charts';
+
+const DisplayAll_VG = (props) => {
+
+    console.log(props.videogames)
+
+    return({})
+}
+
+export default DisplayAll_VG;
