e9790e4d7513b61cba427930cbc83d5abaeb0a9a
Jalen Arrington <jmarrington178@yahoo.com>
2022-10-05 16:26:44 -0400

    Began working on an overhead navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h3>Hello World</h3>
     </div>
   );
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <div className="navBar">
+            <ul>
+                <li className="brand">
+                    <div style={{ textDecoration: "none", color: "white" }}>
+                        <b>Game Data</b>
+                    </div>
+                </li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Navbar;
