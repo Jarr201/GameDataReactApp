045632c4aa9ed6f611102f8b71518de9ffc5a325
irishandyesiam <nathan.irish@gmail.com>
2022-10-05 15:01:25 -0500

    Created SearchBar component. Have errors to resolve.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios'
 // import DisplayAllVG from './Components/DisplayAllVG/DisplayAllVG';
+import SearchBar from './Components/SearchBar/SearchBar';
 import './App.css';
 
 
@@ -24,12 +25,23 @@ function App() {
     setVideogames(response.data);
     console.log(response.data);
   }
+
+  function getSearchedVideoGame(searchterm){
+    let response = videogames.filter(function(e){
+      if (e.name.includes(searchterm))
+      {return true}
+      else
+      {return false}
+    });
+    setVideogames(response)
+  }
   
 
   return (
     <div>
       <h3>Hello World</h3>
       {/* <DisplayAllVG videogames={videogames}/> */}
+      <SearchBar submittedSearchTerm={getSearchedVideoGame} />
     </div>
   );
 }
